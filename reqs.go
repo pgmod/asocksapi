164e13766bb729266cb51d1838c212af32266cc2
Андрей Шкарёв <wex335games@yandex.ru>
2024-11-11 17:05:36 +0300

    change datatype for Count param

--- reqs.go
+++ reqs.go
@@ -9,5 +9,5 @@ type CreatePortsReq struct {
 	ProxyTypeId      ProxyTypeId      `json:"proxy_type_id"`
 	Name             string           `json:"name"`
 	ServerPortTypeId ServerPortTypeId `json:"server_port_type_id"`
-	Count            string           `json:"count"`
+	Count            int              `json:"count"`
 }
